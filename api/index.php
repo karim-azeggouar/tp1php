d4156d5cb1c2ed1fb13585e3562bd859de34755c
Azeggouar <k.azeggouar@gmail.com>
2026-03-27 09:55:02 +0100

    awal commit

---
+++ api/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>  Dev101 portfolio     </h1>
+  
+
+
+<h2>   Ateliers PHP </h2>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
